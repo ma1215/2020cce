5994954ad25a9158ee3c98226297a99e82e976a6
ma1215 <79689048+ma1215@users.noreply.github.com>
2021-03-12 14:25:10 +0800

    Add files via upload

---
+++ week03實習1.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int a[10];
+int main ()
+{
+	int n;
+	scanf("%d",&n);
+	int ans=1;
+	for(int i=1;i<=n;i++)
+	{
+		scanf("%d",&a[i]);
+		ans=a[i]*a[i];
+		printf("%d,",ans);
+	}
+	printf("\n");
+}
